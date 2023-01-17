27dcaef28fb0c77327d87338d6b5807bc834c0eb
Tarik-Harda <tharda14@gmail.com>
2023-01-17 18:52:14 +0100

    Design Activity & add identification expect textview

--- app/src/main/java/com/example/multiexcerice/MainActivity.java
+++ app/src/main/java/com/example/multiexcerice/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.multiexcerice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_renitializer,btn_afficher,btn_quitter;
+    EditText ed_number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Exercise 2 : Multiplication");
+
+        btn_renitializer=findViewById(R.id.btn_renitialiser);
+        btn_afficher=findViewById(R.id.btn_afficher);
+        btn_quitter=findViewById(R.id.bnt_quitter);
+        ed_number=findViewById(R.id.ed_number);
+
     }
 }
